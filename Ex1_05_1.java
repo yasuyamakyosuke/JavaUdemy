4c608b5c82bdff25283115e797998c080644841a
Yasuyamakyosuke <toyojindai@gmail.com>
2020-11-08 22:50:43 +0900

    ex5

---
+++ Ex1_05_1.java
@@ -0,0 +1,18 @@
+class Ex_05_1 {
+	public static void main (String[] args){
+		int income = 5000000 ;
+		System.out.println("年収：" + income);
+
+		long maketSize = 300000000L ;
+		System.out.println("市場規模：" + maketSize);
+
+		char likeKanzi = '山' ;
+		System.out.println("好きな漢字：" + likeKanzib);
+
+		String wantJob = "エンジニア" ;
+		System.out.println("なりたい職業：" + wantJob);
+
+		boolean guraduation = true ;
+		System.out.println("卒業済みか：" true );
+	}
+}
